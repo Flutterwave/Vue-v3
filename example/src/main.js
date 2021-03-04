4146f577935bd878aae6b04306ac7fd0f0b077d2
Cornelius Ashley-Osuzoka <59456456+Corvus97@users.noreply.github.com>
2021-03-04 13:00:01 +0100

    masked public key

--- example/src/main.js
+++ example/src/main.js
@@ -4,7 +4,7 @@ import Flutterwave from 'flutterwave-vue-v3'
 
 Vue.config.productionTip = false
 
-Vue.use( Flutterwave, { publicKey: 'FLWPUBK_TEST-0b04581***X' } ) //use your public key here
+Vue.use( Flutterwave, { publicKey: 'FLWPUBK_TEST-xxxxxxxxxxxxxxxxxxxxx-X' } ) //use your public key here
 
 new Vue({
   render: h => h(App),
