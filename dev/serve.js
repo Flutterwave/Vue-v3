891d043d67ca54b83d953063d9dce56f14882607
Cornelius Ashley-Osuzoka <59456456+Corvus97@users.noreply.github.com>
2021-03-04 12:59:26 +0100

    masked public key

--- dev/serve.js
+++ dev/serve.js
@@ -4,7 +4,7 @@ import Flutterwave from "../src/entry";
 
 Vue.config.productionTip = false;
 
-Vue.use(Flutterwave, { publicKey: 'FLWPUBK_TEST-0b04581c8d73fd08d5c720e1e0f803b4-X' } )
+Vue.use(Flutterwave, { publicKey: 'FLWPUBK_TEST-xxxxxxxxxxxxxxxxxxxxx-X' } )
 
 
 
